8c44220a982b39069819dead63fa621277a63a91
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-02-02 11:02:03 +0530

    Create down_counter_tb.v

---
+++ sequential logic/down counter/down_counter_tb.v
@@ -0,0 +1,23 @@
+// Code your testbench here
+// or browse Examples
+module downcounter_testbench();
+reg clk, reset;
+wire [3:0] counter;
+
+down_counter dut(clk, reset, counter);
+initial begin 
+clk=0;
+forever #5 clk=~clk;
+end
+initial begin
+reset=1;
+#20;
+reset=0;
+end
+initial
+    begin
+      $dumpfile("dump.vcd"); 
+      $dumpvars(1);
+      #10000$finish;
+    end
+endmodule 
