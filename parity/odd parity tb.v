38b2ff1fead223d5960f43a4de30e5b436397ad1
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-02-05 12:29:57 -0800

    Create odd parity tb.v

---
+++ parity/odd parity tb.v
@@ -0,0 +1,37 @@
+module parity_tb;
+  reg x,y,z;
+  wire result;
+  parity uut (.x(x),.y(y),.z(z),.result(result));
+  initial begin
+    x = 0;y = 0;z = 0;
+#100;
+x = 0;
+y = 0;
+z = 1;
+#100;
+x = 0;
+y = 1;
+z = 0;
+#100;
+x = 0;
+y = 1;
+z = 1;
+#100;
+x = 1;
+y = 0;
+z = 0;
+#100;
+x = 1;
+y = 0;
+z = 1;
+#100;
+x = 1;
+y = 1;
+z = 0;
+#100;
+x = 1;
+y = 1;
+z = 1;
+#100;
+end
+endmodule
