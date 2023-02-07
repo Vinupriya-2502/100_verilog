51244cec5cea255427b4f2e4f5297573c61d4876
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-02-07 09:06:06 +0530

    Create bcd to 7segment tb.v

---
+++ bcd to 7segment/bcd to 7segment tb.v
@@ -0,0 +1,23 @@
+module tb_segment7;
+
+    reg [3:0] bcd;
+    wire [6:0] seg;
+    integer i;
+    segment7 uut (
+        .bcd(bcd), 
+        .seg(seg)
+    );
+
+    initial begin	
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    end
+    initial begin
+      for(i = 0;i < 9;i = i+1) 
+        begin
+            bcd = i; 
+            #10;
+        end     
+    end
+      
+endmodule
