a028f7edb342fd1f33d732a6f7adbf5d8e492b45
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-02-13 23:27:48 +0530

    Create block.v

---
+++ sequential logic/block/block.v
@@ -0,0 +1,13 @@
+module block_(din, clk, a, b, c);
+input din;
+input clk;
+output reg a;
+output reg b;
+output reg c;
+always @(posedge clk)
+begin
+      	a = din;
+        b = a;
+        c = b;
+end
+endmodule
