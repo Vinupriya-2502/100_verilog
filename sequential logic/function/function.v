ca830f15d24a708ea88873d8e651bee7c2166e75
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-02-14 13:01:03 +0530

    Create function.v

---
+++ sequential logic/function/function.v
@@ -0,0 +1,16 @@
+// Code your design here
+module fun (a, b, c, d, e, f);
+
+input a,b,c,d,e;
+output wire f;
+
+function call;
+       input a,b,c,d;	
+	begin 
+	call = ((a+b)+(c-d));
+end
+endfunction
+
+assign f = (call(a,b,c,d)) ? e:0;
+
+endmodule
